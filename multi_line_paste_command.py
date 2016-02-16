65787d2650e374d7a78862952e181511d8359e63
Shuky Chen <shuky@fire.glass>
2016-02-16 18:53:00 +0200

    Fixing extra line split

--- multi_line_paste_command.py
+++ multi_line_paste_command.py
@@ -5,6 +5,7 @@ class MultiLinePasteCommand(sublime_plugin.TextCommand):
     """Go to definition command"""
     def run(self, edit):
         self.view.run_command("paste")
-        for i in sublime.get_clipboard().split("\n"):
+
+        for i in range(len(sublime.get_clipboard().split("\n")) -1):
             self.view.run_command("select_lines", { "forward": False })
         self.view.run_command("move_to", {"to": "eol"})
